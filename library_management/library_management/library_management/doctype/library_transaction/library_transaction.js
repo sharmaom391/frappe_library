560a5eb6676c2d06420f5f82106bf24129a7c818
om sharma <sharmaom391@gmaiil.com>
2020-03-29 19:44:56 +0530

    library_management

---
+++ library_management/library_management/library_management/doctype/library_transaction/library_transaction.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2020, Frappe and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Library Transaction', {
+"library_member" : function(frm) {
+        frappe.call({
+            "method": "frappe.client.get",
+            args: {
+                doctype: "Library Member",
+                name: frm.doc.library_member
+            },
+            callback: function (data) {
+                frappe.model.set_value(frm.doctype,
+                    frm.docname, "member_name",
+                    data.message.first_name
+                    + (data.message.last_name ?
+                        (" " + data.message.last_name) : ""))
+            }
+        })
+    }
+	refresh: function(frm) {
+
+	}
+});
